c5de830fb1a784d6ca640195ac7a771a82a1b874
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2024-11-25 17:44:05 +0100

    Add html script

---
+++ script.js
@@ -0,0 +1,33 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const folderPath = "./app";
+const scriptTag = '<script src="script.js"></script>';
+
+async function addScriptTagToHtmlFiles() {
+  const files = await fs.readdir(folderPath);
+
+  for (const file of files) {
+    if (path.extname(file) !== ".html") continue;
+
+    const filePath = path.join(folderPath, file);
+    const data = await fs.readFile(filePath, "utf8");
+
+    if (data.includes(scriptTag)) {
+      continue;
+    }
+
+    const updatedContent = data.includes("</body>")
+      ? data.replace(/<\/body>/i, `${scriptTag}\n</body>`)
+      : `${data}\n${scriptTag}`;
+
+    if (!data.includes("</body>")) {
+      console.log(`🔴 No </body> found in: ${file}`);
+    }
+
+    await fs.writeFile(filePath, updatedContent, "utf8");
+    console.log(`Script tag added to: ${file}`);
+  }
+}
+
+addScriptTagToHtmlFiles();
